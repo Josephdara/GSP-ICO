5cf672f91f9b01c058e2fb11f205fa2655a8e619
Joseph Dara <jsatimehin@gmail.com>
2022-10-02 04:30:08 +0100

    debug

--- ico-dapp/pages/index.js
+++ ico-dapp/pages/index.js
@@ -1,9 +1,15 @@
 import { BigNumber, Contract, utils } from "ethers";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
 import { Web3Modal } from "web3modal";
-import { GSP_CONTRACT_ABI, GST_CONTRACT_ADDRESS } from "../constant";
+import {
+  GSP_CONTRACT_ABI,
+  GST_CONTRACT_ADDRESS,
+  NFT_CONTRACT_ABI,
+  NFT_CONTRACT_ADDRESS,
+} from "../constant";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -13,6 +19,7 @@ export default function Home() {
   const [balanceOfGSP, setBalanceOfGSP] = useState(zero);
   const [tokenAmount, setTokenAmount] = useState(zero);
   const [loading, setLoading] = useState(false);
+  const [unclaimedTokens, setUnclaimedTokens] = useState(zero);
   const web3ModalRef = useRef();
 
   async function connectWallet() {
@@ -47,7 +54,7 @@ export default function Home() {
       );
       const address = await signer.getAddress();
       const balance = await GSTContract.balanceOf(address);
-      setBalanceOfGSP(balance)
+      setBalanceOfGSP(balance);
     } catch (error) {
       console.error(error);
     }
@@ -62,7 +69,7 @@ export default function Home() {
       );
 
       const balance = await GSTContract.totalSupply();
-      setTokenMinted(getTotalMinted)
+      setTokenMinted(getTotalMinted);
     } catch (error) {
       console.error(error);
     }
@@ -85,11 +92,79 @@ export default function Home() {
       window.alert("Welcome, You now own Glory Sound Tokens");
       await getGSPBalance();
       await getTotalMinted();
+      await unclaimedTokens()
     } catch (error) {
       console.error(error);
     }
   }
+  async function getTokensToBeClaimed() {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+      const signer = await getProviderOrSigner(true)
+      const address = await signer.getAddress()
+      const balance = await nftContract.balanceOf(address)
+      if(balance === zero){
+        setUnclaimedTokens(zero)
+      } else  {
+        var amount = 0;
+        for (var i = 0; i < balance; i++){
+          const tokenID = await nftContract.tokenOfOwnerByIndex(address, i)
+          const claimed =  await GSTContract.tokenIDsClaimed(tokenID)
+          if(!claimed){
+            amount++
+          }
+
+        }
+      } 
+    } catch (error) {
+      console.error(error);
+      setUnclaimedTokens(zero)
+    }
+  }
+
+
+  const claimGST = async () =>{
+    try {
+      const signer = await getProviderOrSigner(true);
+      const GSTContract = new Contract(
+        GST_CONTRACT_ADDRESS,
+        GSP_CONTRACT_ABI,
+        signer
+      );
+      const tx = await GSTContract.claim()
+      setLoading(true)
+      await tx.wait()
+      setLoading(false)
+      window.alert("Claim to Glory Sound Token Successful")
+      await getGSPBalance();
+      await getTotalMinted();
+      await unclaimedTokens()
+      
+    } catch (error) {console.error(error);
+      
+    }
+  }
   async function renderButton() {
+    if (loading) {
+      return (
+        <div>
+          <button className={styles.button}>Loading...</button>
+        </div>
+      );
+    }
+    if(unclaimedTokens){
+      return(<div>
+        <div className={styles.description}>
+          {unclaimedTokens * 50} GST to be claimed
+        </div>
+        <button onClick={claimGST} className= {styles.button}> Claim GST</button>
+      </div>)
+    }
     return (
       <div className={{ display: "flex-col" }}>
         <div>
@@ -121,8 +196,11 @@ export default function Home() {
         disableInjectedProvider: false,
       });
       connectWallet();
+       getGSPBalance();
+       getTotalMinted();
+       unclaimedTokens()
     }
-  }, []);
+  }, [walletConnected]);
 
   return (
     <div>
@@ -130,10 +208,10 @@ export default function Home() {
         <title> Glory Sound Token ICO</title>
         <meta name="description" content="GST ICO" />
       </Head>
-      <div className={style.main}>
+      <div className={styles.main}>
         <div>
-          <h1 className={style.title}>Welcome to Glory Sound Token</h1>
-          <div className={style.description}>
+          <h1 className={styles.title}>Welcome to Glory Sound Token</h1>
+          <div className={styles.description}>
             NFT owners can claim, New users can mint GST{" "}
           </div>
           {walletConnected ? (
@@ -152,7 +230,13 @@ export default function Home() {
             </button>
           )}
         </div>
+        <div>
+          <img className={styles.image} src= "./gsp.png"></img>
+        </div>
       </div>
+      <footer className={styles.footer}>
+        Made by josephdara.eth
+      </footer>
     </div>
   );
 }
